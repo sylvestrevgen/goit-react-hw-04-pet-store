4f2b23f58ca59db05b191a0337295f560310d6ba
sylvestrevgen <sylvestrevgen@gmail.com>
2019-10-02 11:55:13 +0300

    Updates

---
+++ precache-manifest.b144569c54e9dca5591b678668e49207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf906ecdf08dc934650eacc2d8f068cb",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "3e500c1e20a5a56ebc29",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.55f4e502.chunk.css"
+  },
+  {
+    "revision": "cfa53753b5617338e7a6",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.ab0b181e.chunk.js"
+  },
+  {
+    "revision": "3e500c1e20a5a56ebc29",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.7bb10c9a.chunk.js"
+  },
+  {
+    "revision": "61b4497bce358e413217",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.537c8fd8.js"
+  }
+]);
