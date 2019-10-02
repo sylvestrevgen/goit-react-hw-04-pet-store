91fc8362394d6eae7d9674b4e11a3095f4f7d2af
sylvestrevgen <sylvestrevgen@gmail.com>
2019-10-02 11:54:01 +0300

    use hashrouter instead browserrouter for gh pages deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
